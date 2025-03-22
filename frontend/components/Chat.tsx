baf4ca57e511f9041b8fc2bc891b3f8062c114b8
Htgar <development@htgar.org>
2025-03-22 17:04:28 +0800

    Added frontend

---
+++ frontend/components/Chat.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useChat } from "ai/react";
+
+import { Chat } from "@/components/ui/chat";
+
+export function ChatUI() {
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    append,
+    isLoading,
+    stop,
+  } = useChat((api = "localhost:8000/chat"));
+
+  return (
+    <Chat
+      messages={messages}
+      input={input}
+      handleInputChange={handleInputChange}
+      handleSubmit={handleSubmit}
+      isGenerating={isLoading}
+      stop={stop}
+      append={append}
+      suggestions={[
+        "Generate a tasty vegan lasagna recipe for 3 people.",
+        "Generate a list of 5 questions for a frontend job interview.",
+        "Who won the 2022 FIFA World Cup?",
+      ]}
+    />
+  );
+}
